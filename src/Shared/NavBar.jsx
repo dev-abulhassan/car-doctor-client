14e3981a7532302cfe44505938b441bd4d25be5b
dev-abulhassan <abulhassan.dev@gmail.com>
2023-05-14 03:34:25 +0600

    banner section complete

--- src/Shared/NavBar.jsx
+++ src/Shared/NavBar.jsx
@@ -21,16 +21,20 @@ const NavBar = () => {
                <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                   {navItem}
                </ul>
+
             </div>
             <Link to={'/'}>
                <img src={logo} alt="" />
             </Link>
          </div>
+
+
          <div className="navbar-center hidden lg:flex">
             <ul className="menu menu-horizontal px-1">
                {navItem}
             </ul>
          </div>
+
          <div className="navbar-end">
             <Link to={'/'} class="relative inline-flex items-center justify-start  border-2 border-[#FF3811]  px-6 py-3 overflow-hidden font-medium transition-all bg-white rounded hover:bg-white group">
                <span class="w-48 h-48 rounded rotate-[-40deg] bg-[#FF3811] absolute bottom-0 left-0 -translate-x-full ease-out duration-500 transition-all translate-y-full mb-9 ml-9 group-hover:ml-0 group-hover:mb-32 group-hover:translate-x-0"></span>
